7f7d356a8f1f714a4e446baecc62b734d1cb0f3b
Scott Strickland <sstrickland@sycomtech.com>
2018-01-25 15:55:13 -0500

    Add files via upload

---
+++ mkdir.py
@@ -0,0 +1,21 @@
+#!/etc/bin/env python3
+import os, sys
+
+#path_home = "c:\pm"
+#print(path_home)
+
+#new_folder = 'new_stuff'
+#print(new_folder)
+
+#new_path = os.path.join(path_home, new_folder)
+#print(new_path)
+
+def make_new_folder(folder):
+    """To make a new folder"""
+    if os.path.isdir(folder):
+        print(str(make_new_folder) + ' already exists.')
+        pass
+    else:
+        os.mkdir(folder)
+
+make_new_folder("c:/pm/new_stuff")
